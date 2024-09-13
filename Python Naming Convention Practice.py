670528145fe8991b8499a597aaf098d368555a4f
ElMightyVato <jairgranadosramirez@gmail.com>
2024-09-13 00:20:21 -0500

    Update Python Naming Convention Practice.py

--- Python Naming Convention Practice.py
+++ Python Naming Convention Practice.py
@@ -2,7 +2,7 @@
 # several variable and constant names that don't follow the Python naming convention.
 #  Your task is to correct them:
 PI = 3.14
-User_Age = 25
-User_Location = "New York"
+User_age = 25
+User_location = "New York"
 MAX_LIMIT = 1000
 
