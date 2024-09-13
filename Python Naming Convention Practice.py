bd6bf2256284e9d55cf196f943a7051af79c813d
ElMightyVato <jairgranadosramirez@gmail.com>
2024-09-13 00:19:20 -0500

    Update Python Naming Convention Practice.py

--- Python Naming Convention Practice.py
+++ Python Naming Convention Practice.py
@@ -6,5 +6,3 @@ User_Age = 25
 User_Location = "New York"
 MAX_LIMIT = 1000
 
- naming convention for variables is snake_case and for constants is SNAKE_CASE, user location
-and user age are variables, pi value and max limit are constants, so in the exercise 2. Python Naming Convention Practice you have to use the correct names.
